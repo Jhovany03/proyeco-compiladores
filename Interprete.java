d3858c913f9d4ef7aabf157e39e3cdc5bfe542c7
Sebas <e.sampietro14@gmail.com>
2023-03-30 22:37:17 -0600

    Actualización de los demás archivos

--- Interprete.java
+++ Interprete.java
@@ -1,5 +1,3 @@
-package mx.ipn.escom.compiladores;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -48,7 +46,7 @@ public class Interprete {
 
     private static void ejecutar(String source){
         Scanner scanner = new Scanner(source);
-        List<Token> tokens = scanner.scanTokens();
+        //List<Token> tokens = scanner.scanTokens(token, lexema, literal, linea);
 
         for(Token token : tokens){
             System.out.println(token);
--- TipoToken.java
+++ TipoToken.java
@@ -1,5 +1,3 @@
-package mx.ipn.escom.compiladores;
-
 public enum TipoToken {
     // Crear un tipoToken por palabra reservada
     // Crear un tipoToken: identificador, una cadena y numero
@@ -7,7 +5,12 @@ public enum TipoToken {
 
 
     // Palabras clave:
-    Y, CLASE,
+    ELSE, LONG, SWITCH, BREAK, ENUM, REGISTER, TYPEDEF, CASE, EXTERN, RETURN, UNION, CHAR, FLOAT, SHORT, UNSIGNED,
+    CONST, FOR, SIGNED, VOID, CONTINUE, GOTO, SIZEOF, VOLATILE, DEFAULT, IF, STATIC, WHILE, DO, INT, STRUCT, _PACKED,
+    DOUBLE,
+    //Caracteres especiales
+    OPCOMPARACION, PARABRE, PARCIERRA, CORABRE, CORCIERRA, LLAVEABRE, LLAVECIERAA, CADENA, IDENTIFICADOR, NUMERO,
+    COMA, PUNTO, PUNTOYCOMA, OPARITMETICOS, IGUAL,
 
     // Final de cadena
     EOF
